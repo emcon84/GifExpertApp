dc0a31f9e831f88c0dd5386c2b34cef568162efd
Emiliano Conti <emcon84@gmail.com>
2021-10-02 08:58:11 -0300

    gifExpertApp

---
+++ src/components/GifGrid.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+import { GifGridItem } from './GifGridItem'
+
+
+const GifGrid = ({ category }) => {
+    
+
+    const { data:images, loading} = useFetchGifs( category );
+   
+
+  return (
+    <>
+      <h3> {category} </h3>
+
+      <div className="card-grid">
+
+        {loading && <p>Loading...</p>}
+      
+            {
+            
+              images.map(img => (
+                <GifGridItem 
+                  key={img.id}
+                  {...img}
+                />
+
+              ))
+            }
+        
+      </div>
+    </>
+  );
+};
+
+export default GifGrid;
